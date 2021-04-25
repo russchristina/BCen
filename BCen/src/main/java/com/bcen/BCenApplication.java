3d18ce25229d3b8bbf24f9fbe5b9259ff589ae5f
russ.christinas <russ.christinas@gmail.com>
2021-04-24 20:28:14 -0500

    back and frontend skeletons

---
+++ BCen/src/main/java/com/bcen/BCenApplication.java
@@ -0,0 +1,13 @@
+package com.bcen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BCenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BCenApplication.class, args);
+	}
+
+}
---
+++ BCen/src/test/java/com/bcen/BCenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bcen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BCenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

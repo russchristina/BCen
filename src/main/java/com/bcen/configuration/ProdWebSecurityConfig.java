788aab64105f008bb939ab6d3d6321d369d43803
russ.christinas <russ.christinas@gmail.com>
2021-04-26 00:01:00 -0500

    trying https

--- src/main/java/com/bcen/configuration/ProdWebSecurityConfig.java
+++ src/main/java/com/bcen/configuration/ProdWebSecurityConfig.java
@@ -14,7 +14,7 @@ public class ProdWebSecurityConfig implements WebMvcConfigurer{
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-		.allowedOrigins("http://bcen.herokuapp.com");
+		.allowedOrigins("https://bcen.herokuapp.com");
 	}
 
 }

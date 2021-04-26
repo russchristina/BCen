439447a526c75e8386b1319662913c3eab9d96ff
russ.christinas <russ.christinas@gmail.com>
2021-04-25 23:05:48 -0500

    adding functionality for profiles

---
+++ src/main/java/com/bcen/configuration/WebSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.bcen.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@Profile(value = "prod")
+public class WebSecurityConfig implements WebMvcConfigurer{
+	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/*")
+		.allowedOrigins("http://bcen.herokuapp.com");
+	}
+
+}

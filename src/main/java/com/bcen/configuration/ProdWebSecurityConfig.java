1750fdc58a2feeba63a7652f2e04eda3581ef273
russ.christinas <russ.christinas@gmail.com>
2021-05-09 08:42:26 -0500

    ...

--- src/main/java/com/bcen/configuration/ProdWebSecurityConfig.java
+++ src/main/java/com/bcen/configuration/ProdWebSecurityConfig.java
@@ -24,7 +24,6 @@ public class ProdWebSecurityConfig extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
 		http.cors();
-		http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
 	}
 	
 	@Bean

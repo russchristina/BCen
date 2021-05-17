11036669c54ae1eea7c0ecef7025a167f62a7295
russ.christinas <russ.christinas@gmail.com>
2021-05-16 22:08:50 -0500

    cors

--- src/main/java/com/bcen/configuration/ProdWebSecurityConfig.java
+++ src/main/java/com/bcen/configuration/ProdWebSecurityConfig.java
@@ -31,7 +31,7 @@ public class ProdWebSecurityConfig extends WebSecurityConfigurerAdapter{
 		CorsConfiguration configuration = new CorsConfiguration();
 		configuration.setAllowedOrigins(Arrays.asList("https://bcen.herokuapp.com", "http://bcen.herokuapp.com"));
 		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS", "HEAD"));
-		configuration.setExposedHeaders(Arrays.asList("Access-Control-Allow-Origin"));
+//		configuration.setExposedHeaders(Arrays.asList("Access-Control-Allow-Origin"));
 		configuration.setAllowedHeaders(Arrays.asList("*"));
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", configuration);
